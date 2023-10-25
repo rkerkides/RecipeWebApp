0f9c9897c38ddcd156adeee1bc844056e25a0cf8
Renos Kerkides <rkerkides@gmail.com>
2023-10-25 23:39:40 +0100

    Create IngredientInitializer

---
+++ src/main/java/com/rkerkides/recipewebapp/initializer/IngredientInitializer.java
@@ -0,0 +1,30 @@
+package com.rkerkides.recipewebapp.initializer;
+
+import com.rkerkides.recipewebapp.entity.Ingredient;
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.Column;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import com.rkerkides.recipewebapp.service.IngredientService;
+
+@Component
+public class IngredientInitializer {
+
+    @Autowired
+    private IngredientService ingredientService;
+
+    @PostConstruct
+    public void initData() {
+        // Create and save the ingredients
+        Ingredient ingredient1 = new Ingredient();
+        ingredient1.setName("Sample Ingredient 1");
+        ingredientService.save(ingredient1);
+
+        Ingredient ingredient2 = new Ingredient();
+        ingredient2.setName("Sample Ingredient 2");
+        ingredientService.save(ingredient2);
+
+    }
+
+
+}
