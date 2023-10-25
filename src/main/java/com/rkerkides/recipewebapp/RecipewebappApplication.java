324254117244f9c26eeb5ab3410a6c42acbf4028
Renos Kerkides <rkerkides@gmail.com>
2023-10-25 22:07:50 +0100

    Initialize Spring Boot and Vite React Projects

---
+++ src/main/java/com/rkerkides/recipewebapp/RecipewebappApplication.java
@@ -0,0 +1,13 @@
+package com.rkerkides.recipewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipewebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipewebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rkerkides/recipewebapp/RecipewebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkerkides.recipewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipewebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
