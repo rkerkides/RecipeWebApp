5c82acacf962e5b1827f751f437c87114cbb6a4e
Renos Kerkides <rkerkides@gmail.com>
2023-10-26 17:26:51 +0100

    Refine HomePage component layout
    Introduced a refined layout for the homepage component. Added placeholder text and images, which will be replaced with actual content in future commits.

--- src/main/resources/React/src/components/HomePage.jsx
+++ src/main/resources/React/src/components/HomePage.jsx
@@ -1,11 +1,57 @@
 import React from "react";
+import {
+  Container,
+  Typography,
+  Grid,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Button,
+} from "@mui/material";
 
 const HomePage = () => {
   return (
-    <div className="home-page">
-      <h2>Welcome to My Blog</h2>
-      <p>This is the homepage of my awesome blog.</p>
-    </div>
+    <Container>
+      {/* Header */}
+      <Typography variant="h1" align="center" gutterBottom>
+        My Food Recipe Blog
+      </Typography>
+
+      {/* Hero Section */}
+      <Typography variant="h4" align="center" paragraph>
+        Welcome to the ultimate food recipe destination!
+      </Typography>
+
+      {/* Recipe Cards */}
+      <Grid container spacing={4}>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((item) => (
+          <Grid item xs={12} sm={6} md={4} key={item}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  alt="Recipe Image"
+                  height="140"
+                  image={`https://source.unsplash.com/random?food,${item}`}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Recipe {item}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    This is a sample description for Recipe {item}.
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <Button size="small" color="primary">
+                Learn More
+              </Button>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 };
 
