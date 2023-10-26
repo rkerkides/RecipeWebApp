c6a19db2370bd0a0ba0d742f62e0ec110c055fab
Renos Kerkides <rkerkides@gmail.com>
2023-10-26 17:07:34 +0100

    Create HomePage component

---
+++ src/main/resources/React/src/components/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <h2>Welcome to My Blog</h2>
+      <p>This is the homepage of my awesome blog.</p>
+    </div>
+  );
+};
+
+export default HomePage;
