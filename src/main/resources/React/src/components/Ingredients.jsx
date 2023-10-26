61c980331e06dde19bc35624f69f7c548a0970e1
Renos Kerkides <rkerkides@gmail.com>
2023-10-26 17:09:17 +0100

    Create Ingredients and Recipes components

---
+++ src/main/resources/React/src/components/Ingredients.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const IngredientsList = () => {
+  return (
+    <div>
+      <h1>Ingredients list Page</h1>
+      {/* Your ingredients content here */}
+    </div>
+  );
+};
+
+export default IngredientsList;
---
+++ src/main/resources/React/src/components/Recipes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Recipes = () => {
+  return (
+    <div>
+      <h1>Recipes Page</h1>
+      {/* Your recipes content here */}
+    </div>
+  );
+};
+
+export default Recipes;
