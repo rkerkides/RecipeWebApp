ad63e53e70e75f4a177d5dcaa7950e90e5b6fa4c
Renos Kerkides <rkerkides@gmail.com>
2023-10-26 17:12:52 +0100

    Update App.jsx to utilize components

--- src/main/resources/React/src/App.jsx
+++ src/main/resources/React/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom"; // Make sure this line is correct
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import HomePage from "./components/HomePage";
+import IngredientsList from "./components/Ingredients";
+import Recipes from "./components/Recipes";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+        minWidth: "100vw",
+      }}
+    >
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/ingredients" element={<IngredientsList />} />
+          <Route path="/recipes" element={<Recipes />} />
+        </Routes>
+      </BrowserRouter>
+      <div style={{ flex: 1, width: "100%" }}></div>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
