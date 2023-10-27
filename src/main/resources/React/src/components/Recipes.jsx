b0fc2bc34affaba8b35a7e29cc670c5a478de520
Renos Kerkides <rkerkides@gmail.com>
2023-10-27 16:36:37 +0100

    Update Recipes component to display recipe info

--- src/main/resources/React/src/components/Recipes.jsx
+++ src/main/resources/React/src/components/Recipes.jsx
@@ -1,12 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const Recipes = () => {
+const RecipesList = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/recipes") // Spring Boot API url for recipes
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setRecipes(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  const fetchImage = (id) => {
+    return `http://localhost:8080/recipes/image/${id}`;
+  };
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
     <div>
-      <h1>Recipes Page</h1>
-      {/* Your recipes content here */}
+      <h1>Recipes List Page</h1>
+      <ul>
+        {recipes.map((recipe, index) => (
+          <li key={index}>
+            {recipe.title} - {recipe.description}
+            <img
+              src={fetchImage(recipe.id)}
+              alt={recipe.title}
+              width="50"
+              height="50"
+            />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
 
-export default Recipes;
+export default RecipesList;
