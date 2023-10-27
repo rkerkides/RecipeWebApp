4ed0f5cf51fe3961c9839704f486422ee99fceb2
Renos Kerkides <rkerkides@gmail.com>
2023-10-27 16:34:53 +0100

    Create Recipe service

---
+++ src/main/java/com/rkerkides/recipewebapp/service/RecipeService.java
@@ -0,0 +1,34 @@
+package com.rkerkides.recipewebapp.service;
+
+import com.rkerkides.recipewebapp.entity.Recipe;
+import com.rkerkides.recipewebapp.repository.RecipeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RecipeService {
+
+    @Autowired
+    private RecipeRepository recipeRepository;
+
+    public List<Recipe> findAll() {
+        return recipeRepository.findAll();
+    }
+
+    public Optional<Recipe> findById(Long id) {
+        return recipeRepository.findById(id);
+    }
+
+    public Recipe save(Recipe recipe) {
+        return recipeRepository.save(recipe);
+    }
+
+    public void deleteById(Long id) {
+        recipeRepository.deleteById(id);
+    }
+
+    // Additional methods can be added here if needed
+}
