7a11216ab53fb3e234df668c0f40051bb38c33d8
Renos Kerkides <rkerkides@gmail.com>
2023-10-27 15:36:30 +0100

    Add Getter & Setter using Lombok

--- src/main/java/com/rkerkides/recipewebapp/entity/RecipeIngredient.java
+++ src/main/java/com/rkerkides/recipewebapp/entity/RecipeIngredient.java
@@ -1,8 +1,12 @@
 package com.rkerkides.recipewebapp.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class RecipeIngredient {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
